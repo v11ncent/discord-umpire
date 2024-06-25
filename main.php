71808bd4a442f35fb2c67e16665e7d574312c614
Vince <45806875+v11ncent@users.noreply.github.com>
2024-06-25 00:24:20 -0400

    Starts thread if message contains a 'banned' phrase

--- main.php
+++ main.php
@@ -1,25 +1,63 @@
 <?php
 
-include __DIR__.'/vendor/autoload.php';
+include __DIR__ . '/vendor/autoload.php';
 
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Intents;
 use Discord\WebSockets\Event;
 
+// loads our .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// require our .env variables to be set
+$dotenv
+    ->required(['UMPIRE_TOKEN', 'UMPIRE_GUILD_ID', 'UMPIRE_CHANNEL_ID'])
+    ->notEmpty();
+
+// .env variables for our bot
+$token = $_ENV['UMPIRE_TOKEN'];
+$serverId = $_ENV['UMPIRE_GUILD_ID'];
+$channelId = $_ENV['UMPIRE_CHANNEL_ID'];
+
 $discord = new Discord([
-    'token' => 'bot-token',
-    'intents' => Intents::getDefaultIntents()
-//      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
+    'token' => $token,
+    'intents' => Intents::getDefaultIntents(),
 ]);
 
-$discord->on('ready', function (Discord $discord) {
-    echo "Bot is ready!", PHP_EOL;
+$discord->on('ready', function () use ($discord, $serverId, $channelId) {
+    $server = $discord->guilds->get('id', $serverId);
 
-    // Listen for messages.
-    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        echo "{$message->author->username}: {$message->content}", PHP_EOL;
-        // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+    $discord->on(Event::MESSAGE_CREATE, function (
+        Message $message,
+        Discord $discord,
+    ) {
+        $content = $message->content;
+        $phrases = [ // feature: topic analysis instead of phrases
+            'baseball', 'phils', 'phillies',
+            'harper', 'trea', 'ranger',
+            'bohm', 'reserves', 'wheeler',
+            'sanchez', 'strahm', 'marsh',
+            'doomer', 'philly', 'starters',
+            'starter', 'triple play', 'pablo lopez',
+            'peds', 'mlb', 'catcher',
+            'pitcher', 'bat', 'walker',
+            'gave up a run', 'bum', 'taijuan',
+            'yank'
+        ];
+
+        if (\Discord\contains(strtolower($content), $phrases)) {
+            $message->startThread(
+                '⚾ ' . $message->content,
+                60,
+                'Those goblins were talking about baseball!',
+            );
+
+            // creates a date 30s into the future and times out author
+            // $timeoutDuration = \Carbon\Carbon::now()->addSeconds(30);
+            // $message->member->timeoutMember($timeoutDuration);
+        }
     });
 });
 
